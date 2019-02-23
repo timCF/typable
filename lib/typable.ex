52a59f307292d1f200807c672a48a3621a636a65
timcf <timcf@89-235-241-221.saturn.infonet.ee>
2019-02-23 17:49:18 +0200

    it works

--- lib/typable.ex
+++ lib/typable.ex
@@ -1,18 +1,50 @@
-defmodule Typable do
+defprotocol Typable do
   @moduledoc """
-  Documentation for Typable.
+  Protocol which describes type identifier of Elixir term
   """
 
+  @fallback_to_any true
+  @type t :: Typable.t()
+
   @doc """
-  Hello world.
+  Returns module which represents type of given term
 
   ## Examples
 
-      iex> Typable.hello()
-      :world
-
+  ```
+  iex> Typable.type_of(1)
+  Integer
+  iex> Typable.type_of(self())
+  PID
+  iex> Typable.type_of(%URI{})
+  URI
+  ```
   """
-  def hello do
-    :world
+  @spec type_of(t) :: module
+  def type_of(t)
+end
+
+[
+  # scalars
+  Atom,
+  BitString,
+  Float,
+  Function,
+  Integer,
+  PID,
+  Port,
+  Reference,
+  # collections
+  Tuple,
+  List,
+  Map
+]
+|> Enum.each(fn type ->
+  defimpl Typable, for: type do
+    def type_of(_), do: unquote(type)
   end
+end)
+
+defimpl Typable, for: Any do
+  def type_of(%type{}), do: type
 end
--- test/typable_test.exs
+++ test/typable_test.exs
@@ -1,8 +1,4 @@
 defmodule TypableTest do
   use ExUnit.Case
   doctest Typable
-
-  test "greets the world" do
-    assert Typable.hello() == :world
-  end
 end
