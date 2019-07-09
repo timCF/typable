4e8ee7c96e09db852f10d94f6b9562683d11efe0
timcf <timcf@89-235-233-131.saturn.infonet.ee>
2019-07-10 00:23:04 +0300

    Type utilities module, instance_of/1 function

--- lib/typable.ex
+++ lib/typable.ex
@@ -48,3 +48,95 @@ end)
 defimpl Typable, for: Any do
   def type_of(%type{}), do: type
 end
+
+defmodule Type do
+  @moduledoc """
+  Utilities for Typable protocol
+  """
+
+  defdelegate type_of(x), to: Typable
+
+  @doc """
+  Returns term which is instance of given type.
+  Have compile-time guarantees.
+
+  ## Examples
+
+  ```
+  iex> require Type
+  iex> Type.instance_of(Integer)
+  0
+  iex> Type.instance_of(Port)
+  #Port<0.0>
+  iex> Type.instance_of(URI)
+  %URI{}
+
+  iex> quote do
+  ...>   require Type
+  ...>   Type.instance_of(TypeNotExist)
+  ...> end
+  ...> |> Code.eval_quoted()
+  ** (UndefinedFunctionError) function TypeNotExist.__struct__/0 is undefined (module TypeNotExist is not available)
+  ```
+  """
+  defmacro instance_of(quoted_type) do
+    {type, []} = Code.eval_quoted(quoted_type, [], __CALLER__)
+    mk_instance(type)
+  end
+
+  # scalars
+  defp mk_instance(Atom), do: nil
+  defp mk_instance(BitString), do: ""
+  defp mk_instance(Float), do: 0.0
+
+  defp mk_instance(Function) do
+    quote location: :keep do
+      &self/0
+    end
+  end
+
+  defp mk_instance(Integer), do: 0
+
+  defp mk_instance(PID) do
+    quote location: :keep do
+      :erlang.list_to_pid('<0.0.0>')
+    end
+  end
+
+  defp mk_instance(Port) do
+    quote location: :keep do
+      :erlang.list_to_port('#Port<0.0>')
+    end
+  end
+
+  defp mk_instance(Reference) do
+    quote location: :keep do
+      :erlang.list_to_ref('#Ref<0.0.0.0>')
+    end
+  end
+
+  # collections
+  defp mk_instance(Tuple) do
+    quote location: :keep do
+      {}
+    end
+  end
+
+  defp mk_instance(List) do
+    quote location: :keep do
+      []
+    end
+  end
+
+  defp mk_instance(Map) do
+    quote location: :keep do
+      %{}
+    end
+  end
+
+  # other types
+  defp mk_instance(type) do
+    type.__struct__()
+    |> Macro.escape()
+  end
+end
--- test/typable_test.exs
+++ test/typable_test.exs
@@ -1,4 +1,27 @@
 defmodule TypableTest do
   use ExUnit.Case
+  require Type
+  doctest Type
   doctest Typable
+
+  [
+    # scalars
+    Atom,
+    BitString,
+    Float,
+    Function,
+    Integer,
+    PID,
+    Port,
+    Reference,
+    # collections
+    Tuple,
+    List,
+    Map
+  ]
+  |> Enum.each(fn type ->
+    test "type_of/instance_of inversability for #{type}" do
+      assert unquote(type) == unquote(type) |> Type.instance_of() |> Type.type_of()
+    end
+  end)
 end
